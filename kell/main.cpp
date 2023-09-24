c4e370cf9fc1b62dcbbca853848a4a5b2d7ff09c
TolaniMark <124035542+TolaniMark@users.noreply.github.com>
2023-09-24 09:09:21 +0300

    Add files via upload

---
+++ kell/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+main ()
+{
+    int i, n, S=0;
+    bool prim = true;
+
+    cout << "n= ";
+    cin >> n;
+
+    for (i = 2; i <= n/2; i++)
+    {
+        if (n%i == 0)
+        {
+            prim = false;
+            S++;
+        }
+    }
+
+    if (prim)
+    {
+        cout << n << " egy primszam";
+    }
+    else
+    {
+        cout << n << " nem primszam" << endl;
+        cout << "osztoinak szama: " << S;
+    }
+    return 0;
+}
