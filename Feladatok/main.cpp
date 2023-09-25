f73ee323a1515d4905b59494406b322f34bc1913
TolaniMark <124035542+TolaniMark@users.noreply.github.com>
2023-09-25 07:43:23 +0300

    Add files via upload

---
+++ Feladatok/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int n, i, j, s=0;
+
+    cout << "n= ";
+    cin >> n;
+
+    for(i = 1; i<n; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            if (i % j == 0 && j % 2 != 0)
+                s++;
+        }
+
+        if (s == 3)
+        {
+            cout << i << " ";
+        }
+        s=0;
+
+    }
+
+    return 0;
+}
