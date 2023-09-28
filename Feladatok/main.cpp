252f091c79c9415c122b251cfbd646e77fd8be75
TolaniMark <124035542+TolaniMark@users.noreply.github.com>
2023-09-28 10:31:24 +0300

    Add files via upload

---
+++ Feladatok/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, x, p;
+    int hatvany=1, aux = 0, pos = 0;
+
+    cout << "n= ";
+    cin >> n;
+    cout << endl << "(0<x<10) x= ";
+    cin >> x;
+    cout << "(szamjegyek szamaig) p= ";
+    cin >> p;
+
+    while (n>0)
+    {
+    int szamjegy = n%10;
+    n=n/10;
+
+    if (pos != p)
+    {
+    aux = aux + szamjegy * hatvany;
+    }
+    else
+    {
+    aux = aux + x * hatvany;
+    }
+    hatvany *= 10;
+    pos++;
+    }
+    cout << aux << endl;
+
+    return 0;
+}
